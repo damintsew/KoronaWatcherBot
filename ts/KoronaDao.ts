fb4a791cb685e98890dc94c38b606f9a0ee3d9a1
Andrei Damintsev <adamintsev@devexperts.com>
2022-10-17 00:13:53 +0300

    a lof of refactoring

--- ts/KoronaDao.ts
+++ ts/KoronaDao.ts
@@ -10,7 +10,7 @@ export class KoronaDao {
     static async call(countryCode: string): Promise<number> {
         console.log(`Requesting data for country ${countryCode}`)
         try {
-            let {data} = await axios.get(`http://localhost:6666/korona/${countryCode}`);
+            let {data} = await axios.get(`http://localhost:3333/korona/${countryCode}`);
             console.log("Received data: " + data)
             return data.value
 
