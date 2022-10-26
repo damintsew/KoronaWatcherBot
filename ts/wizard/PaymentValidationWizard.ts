f483564fa6ef3f4e99b887737933ac6d5c386d23
Andrei Damintsev <adamintsev@devexperts.com>
2022-10-26 15:12:00 +0300

    tmp

--- ts/wizard/PaymentValidationWizard.ts
+++ ts/wizard/PaymentValidationWizard.ts
@@ -1,98 +1,110 @@
-// noinspection TypeScriptValidateJSTypes
-
-import {MyContext} from "../Domain";
-import {Markup, Scenes} from "telegraf";
 import {PaymentSubscription} from "../entity/PaymentSubscription";
 import moment from "moment";
-import {ds} from "../data-source";
+import {MyConversation, NewContext} from "../bot_config/Domain2";
+import {Keyboard} from "@grammyjs/conversations/out/deps.node";
+import {Service} from "typedi";
+import {PaymentSubscriptionService} from "../service/PaymentSubscriptionService";
 
+export interface Config {
+    subscriptionId: string,
+    subscriptionText: string,
+    price: string,
+
+    onSuccess: any
+}
+
+@Service()
 export class PaymentValidationWizard {
 
-    paymentValidationWizard(): Scenes.WizardScene<MyContext> {
-        return new Scenes.WizardScene<MyContext>(
-            "payment-validation-wizard",
-            async (ctx: MyContext) => {
-                let user = ctx.session.user;
+    constructor(
+        public paymentService: PaymentSubscriptionService
+    ) {
+    }
 
-                if (user.subscriptions == null || user.subscriptions.length == 0) {
-                    await ctx.replyWithMarkdown(
-                        'Данная функциф платная - стоимость подписки 1 usdt/месяц\n' +
-                        'У вас доступна триальная версия - в течении 7 дней.\n' +
-                        'Желаете продолжить ?',
-                        Markup.keyboard([
-                            this.keyboard("Да"),
-                            this.keyboard("Нет")
-                        ]))
-                    //todo move to handle questions
-                    return ctx.wizard.selectStep(1)
-                }
+    private removeKeyb = {remove_keyboard: true};
 
-                const activeTrial = this.findPredicate(user.subscriptions,
-                    s => s.trial == true && s.type == "GARANTEX")
-                if (activeTrial) {
-                    ctx.scene.enter("change-currency-scene")
-                    return;
-                }
+    async trialValidator(conversation: MyConversation, ctx: NewContext, config: Config) {
 
-                const activeGarantexSubscription = this.findPredicate(user.subscriptions,
-                    s => s.type == "GARANTEX")
-                if (activeGarantexSubscription) {
-                    ctx.scene.enter("change-currency-scene")
-                    return;
-                }
+        const activeTrial = this.findPredicate(ctx.user?.subscriptions,
+            s => s.trial && s.type == config.subscriptionId)
 
-                await ctx.replyWithMarkdown(
-                    'Данная функциф платная - стоимость подписки 1 usdt/месяц\n' +
-                    'Пожалуйста переведите 1 usdt на кошелек %номер кошелька% и ОБЯЗАТЕЛЬНО в сообщении укажите Ваш номер пользователя.\n' +
-                    'Ваш номер: ' + user.userId + "\n" +
-                    "Когда отправите нажмите кнопку ниже. Я проверю платеж и вы получите доступ",
-                    Markup.keyboard([
-                        this.keyboard("Деньги отправлены"),
-                        this.keyboard("Отмена")
-                    ]))
-                return ctx.wizard.selectStep(2)
-            },
-            async (ctx: MyContext) => {
-                // @ts-ignore
-                if (ctx.message?.text == "Да") {
-                    const trialSubscription = new PaymentSubscription();
-                    trialSubscription.type ="GARANTEX"
-                    trialSubscription.trial = true
-                    trialSubscription.startDate = new Date()
-                    trialSubscription.expirationDate = moment().add(7, "d").toDate()
-                    trialSubscription.user = ctx.session.user
+        if (!activeTrial) {
+            const keyboard = new Keyboard()
+                .text("Да")
+                .text("Отмена").row()
+                .oneTime()
+                .resized();
+            await ctx.reply(`Данная функциф платная - стоимость подписки ${config.price} usdt/месяц\n` +
+                'У вас доступна триальная версия - в течении 7 дней.\n' +
+                'Желаете продолжить ?', {reply_markup: keyboard})
 
-                    await ds.manager.save(trialSubscription)
-                    await ctx.reply("Триал оформлен. В случае проблем пишите в /support")
-                    ctx.scene.enter("change-currency-scene")
-                } else { // @ts-ignore
-                    if (ctx.message?.text == "Нет") {
-                        ctx.scene.leave()
-                    }
-                }
-            },
-            async (ctx: MyContext) => {
-                // @ts-ignore todo remove ignore
-                if (ctx.message == null || ctx.message.text == null) {
-                    await ctx.reply("Повтрите ввод")
-                    return;
-                }
-                // @ts-ignore todo remove ignore
-                if (ctx.message.text == "Отмена") {
-                    await ctx.reply("Отменяю.", Markup.removeKeyboard())
-                    ctx.scene.leave()
-                    return;
-                }
+            const answer = await conversation.waitFor("message:text");
 
-                // @ts-ignore
-                if (ctx.message?.text == "Деньги отправлены") {
-                    //todo alert admin!!!!
-                }
+            if (answer.msg.text == "Да") {
+                await this.paymentService.createTrialSubscription(config, ctx.user)
+                await ctx.reply("Триал оформлен. В случае проблем пишите в /support")
+                await ctx.reply("Оформление подписки Garantex", config.onSuccess)
+            } else if (answer.msg.text == "Нет") {
+                return ctx.reply("Отменяю", {reply_markup: {remove_keyboard: true}})
             }
-        )
+        }
+
+        if (activeTrial) {
+            await ctx.reply("Оформление подписки Garantex", config.onSuccess)
+            return;
+        }
+
+        const activeGarantexSubscription = this.findPredicate(ctx.user.subscriptions,
+            s => s.type == config.subscriptionId)
+        if (activeGarantexSubscription) {
+            await ctx.reply("Оформление подписки Garantex", config.onSuccess)//{reply_markup: garantexSubscriptionMenu})
+            return;
+        } else {
+            const keyboard = new Keyboard()
+                .text("Да")
+                .text("Отмена").row()
+                .oneTime()
+                .resized();
+
+            await ctx.reply("Ваша подписка кончилась! Оплатите подписку\n" +
+                `Стоимость подписки ${config.price} usdt/месяц.\n
+                Перейти к оплате?`, {reply_markup: keyboard});
+
+            const answer = await conversation.waitFor("message:text");
+
+            if (answer.msg.text == "Да") {
+                const keyboard = new Keyboard()
+                    .text("Перевод отправлен")
+                    .text("Отмена").row()
+                    .oneTime()
+                    .resized();
+
+                await ctx.reply("Переведите на этот кошелек 1 usdt")
+                await ctx.reply("TKH74x3dNGkBnWdFUESPkiGqrscHBgKGNK")
+                await ctx.reply("После перевода. Пришлите мне номер транзакции. В случае проблем пишите /support",
+                    {reply_markup: keyboard})
+
+                let answer = await conversation.waitFor("message:text");
+                if (answer.msg.text == "Перевод отправлен") {
+                    await ctx.reply("Отправьте мне номер транзакции, пожалуйста")
+                    answer = await conversation.waitFor("message:text");
+
+                    // todo save answer
+
+                } else if (answer.msg.text == "Отмена") {
+                    return ctx.reply("Отменяю", {reply_markup: {remove_keyboard: true}})
+                }
+
+            } else if (answer.msg.text == "Нет") {
+                return ctx.reply("Отменяю", {reply_markup: {remove_keyboard: true}})
+            }
+        }
     }
 
     private findPredicate(subscriptions: PaymentSubscription[], predicate: (s: PaymentSubscription) => {}) {
+        if (subscriptions == null) {
+            return null;
+        }
         for (let s of this.findActiveSubscriptions(subscriptions)) {
             if (predicate(s)) {
                 return s
@@ -111,9 +123,5 @@ export class PaymentValidationWizard {
         }
 
         return result;
-    }
-
-    private keyboard(text: string) {
-        return Markup.button.callback(text, "")
     }
 }
