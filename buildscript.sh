704da3b1e704ad431a7a09f56ed344b249ab69a9
Andrei Damintsev <adamintsev@devexperts.com>
2022-10-28 14:32:21 +0300

    tmmp

--- buildscript.sh
+++ buildscript.sh
@@ -1,5 +1,5 @@
-docker buildx build --platform linux/amd64,linux/arm64 . -t damintsew/korona-watcher-bot --push
-docker build . -t damintsew/korona-watcher-bot && docker push damintsew/korona-watcher-bot
+docker buildx build --platform linux/amd64,linux/arm64 . -t damintsew/korona-api --push
+docker build . -t damintsew/korona-api && docker push damintsew/korona-api
 
 
 docker build . -t damintsew/korona-api && docker push damintsew/korona-api
--- ts/KoronaDao.ts
+++ ts/KoronaDao.ts
@@ -19,15 +19,15 @@ export class KoronaDao {
     }
 
     private static getHttpHeader() {
-        return ['Accept: application/vnd.cft-data.v2.86+json',
-            'Cookie: qpay-web/3.0_csrf-token-v2=b15aadd1ec7400786bc9e329f7bdddba; ROUTEID=0adc176d99dd23c4|YzifH; _gali=changeable-field-input-amount; _dc_gtm_UA-100141486-2=1; _dc_gtm_UA-100141486-25=1; _ga=GA1.2.368400859.1664655103; _ga_H68H5PL1N6=GS1.1.1664655103.1.1.1664655108.55.0.0; _gid=GA1.2.1989051445.1664655104; _ym_visorc=b; qpay-web/3.0_locale=en-gb; _ym_d=1664655105; _ym_isad=2; _ym_uid=1664655105320075748; _dc_gtm_UA-100141486-1=1; _dc_gtm_UA-100141486-26=1; _gcl_au=1.1.1138320732.1664655103',
+        return ['Accept: application/vnd.cft-data.v2.94+json',
+            'Cookie: qpay-web/3.0_csrf-token-v2=5b78102a9f14d2b088a08dde4d7240d1; _gali=changeable-field-input-amount; tmr_reqNum=34; ROUTEID=57577586c088f86d|Y1u80; tmr_detect=0%7C1666956491879; _dc_gtm_UA-100141486-2=1; _dc_gtm_UA-100141486-25=1; _ga=GA1.2.235614836.1666956485; _gid=GA1.2.1858527757.1666956485; _ym_visorc=w; tmr_lvid=b277599e0794e482a6bd9147efbc372e; tmr_lvidTS=1665165306717; _ga_H68H5PL1N6=GS1.1.1666956484.1.1.1666956488.56.0.0; qpay-web/3.0_locale=en-gb; _dc_gtm_UA-100141486-1=1; _dc_gtm_UA-100141486-26=1; _ym_isad=2; _gcl_au=1.1.562121422.1666956484; CookieControl={"necessaryCookies":[],"optionalCookies":{"Google":"accepted"},"statement":{"shown":true,"updated":"01/01/2021"},"consentDate":1666784566769,"consentExpiry":90,"interactedWith":true,"user":"36975655-254F-4D28-A8D2-A168675D07E9"}; _ym_d=1666784565; _ym_uid=1664655105320075748; client-id=4b2ada5b-f76d-4bfe-b836-105f21ece7ec',
             'Accept-Encoding: gzip, deflate, br',
             'Host: koronapay.com',
             'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15',
             'Accept-Language: en-gb',
             'Referer: https://koronapay.com/transfers/online/',
             'Connection: keep-alive',
-            'x-csrf-token: b15aadd1ec7400786bc9e329f7bdddba',
+            'x-csrf-token: 5b78102a9f14d2b088a08dde4d7240d1',
             'x-application: Qpay-Web/3.0'];
     }
 }
