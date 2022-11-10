82f3d899c03de9c88bb6dd07521dcec931eb97da
Andrey Damintsev <damintsew@gmail.com>
2022-11-10 21:32:59 +0000

    korona standalone

--- ts/dao/rest/UnistreamDao.ts
+++ ts/dao/rest/UnistreamDao.ts
@@ -1,6 +1,4 @@
-import axios from "axios";
-import * as buffer from "buffer";
-import {Service} from "typedi";
+import {curly} from "node-libcurl";
 
 interface UnistreamResponse {
     "message": null,
@@ -17,19 +15,34 @@ interface UnistreamResponse {
         }]
 }
 
-@Service()
 export class UnistreamDao {
 
     static async call(countryCode: string): Promise<UnistreamResponse> {
         console.log(`Requesting data for country ${countryCode}`)
         try {
-            let {data} = await axios.get(`https://online.unistream.ru/card2cash/calculate?` +
-                `destination=${countryCode}&amount=1000&currency=USD&accepted_currency=RUB&profile=unistream/`);
+            const {
+                statusCode,
+                data
+            } = await curly.get(`https://online.unistream.ru/card2cash/calculate?destination=ISR&amount=1000&currency=USD&accepted_currency=RUB&profile=unistream`,
+                {httpHeader: UnistreamDao.getHttpHeader()});
+            // let {data} = await curly.get(`https://online.unistream.ru/card2cash/calculate?` +
+            //     `destination=${countryCode}&amount=1000&currency=USD&accepted_currency=RUB&profile=unistream/`);
             console.log("Received data: " + data)
             return data as UnistreamResponse
         } catch (err) {
             console.log(err);
             return null
         }
+    }
+
+    private static getHttpHeader() {
+        return ['Accept: application/json, text/javascript, */*; q=0.01',
+            'Origin: https://unistream.ru',
+            'Accept-Encoding: gzip, deflate, br',
+            'Host: online.unistream.ru',
+            'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+            'Accept-Language: ru',
+            'Referer: https://unistream.ru/',
+            'Connection: keep-alive'];
     }
 }
